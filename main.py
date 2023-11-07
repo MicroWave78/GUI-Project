17c2095c798d4821fbe22ded70a8656f29eafbbf
MicroWave78 <dragoschelariu64@gmail.com>
2023-11-07 12:58:12 +0200

    part of the game, will work on it more

--- main.py
+++ main.py
@@ -1,18 +1,65 @@
 import PySimpleGUI as sg
+import time as t
 
-sg.theme('DarkAmber')
 
-layout = [  [sg.Text('Some text on Row 1')],
-            [sg.Text('Enter something on Row 2'), sg.InputText()],
-            [sg.Button('Ok'), sg.Button('Cancel')] ]
+sg.theme('darkpurple1')  # Choose a theme
 
-# Create the Window
-window = sg.Window('Window Title', layout)
-# Event Loop to process "events" and get the "values" of the inputs
+# Initialize variables
+click_count = 0
+click_value = 1
+click_upgrade = 1
+upgrade_cost = 25
+upgrade_level = 1
+
+def formatNum(number):
+    if abs(number) >= 1e15:
+        return f'{number / 1/15:.2f}Q'
+    elif abs(number) >= 1e12:
+        return f'{number / 1e12:.2f}T'
+    elif abs(number) >= 1e9:
+        return f'{number / 1e9:.2f}B'
+    elif abs(number) >= 1e6:
+        return f'{number / 1e6:.2f}M'
+    elif abs(number) >= 1e3:
+        return f'{number / 1e3:.2f}K'
+    else:
+        return str(number)
+
+# Define the layout of the GUI
+layout = [
+    [sg.Text('Click Count:', size=(15, 1)), sg.Text(click_count, size=(10, 1), key='COUNT')],
+    [sg.Button('Click!', key='CLICK')],
+    [sg.Text('Click Value: ', size=(15, 1)), sg.Text(click_value, size=(10, 1), key='VALUE')],
+    [sg.Button(f'Upgrade Click Value [Lv. {upgrade_level}] ({formatNum(upgrade_cost)} clicks)', key='UPGRADE')],
+    [sg.Text('', key = 'COST')],
+    [sg.Button('Quit')]
+]
+
+
+# Create the window
+window = sg.Window('Clicker Game GUI', layout, size = (1000,500))
+
+# Event loop
 while True:
     event, values = window.read()
-    if event == sg.WIN_CLOSED or event == 'Cancel': # if user closes window or clicks cancel
-        break
-    print('You entered ', values[0])
 
+    if event in (sg.WIN_CLOSED, 'Quit'):
+        break
+
+    if event == 'CLICK':
+        click_count += click_value
+        window['COUNT'].update(formatNum(click_count))
+        window['COST'].update('')
+    elif event == 'UPGRADE':
+        if click_count >= upgrade_cost:
+            click_value += click_upgrade
+            click_upgrade += 1
+            click_count -= upgrade_cost
+            upgrade_cost += 8
+            upgrade_level += 1
+            window['VALUE'].update(formatNum(click_value))
+            window['COUNT'].update(formatNum(click_count))
+            window['UPGRADE'].update(f'Upgrade Click Value [Lv. {upgrade_level}] ({formatNum(upgrade_cost)} clicks)')
+        else:
+            window['COST'].update('Not enough clicks.')
 window.close()
