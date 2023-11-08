1a2c7713ca03827c307547dd6306411e7d17eec0
MicroWave78 <131689516+MicroWave78@users.noreply.github.com>
2023-11-08 11:25:45 +0200

    idk what changed but i did change

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 import PySimpleGUI as sg
 import time as t
+import random as r
 
 
 sg.theme('darkpurple1') 
 
 
+font_name = 'Helvetica'
 click_count = 0
-click_value = 1
-click_upgrade = 1
+click_value = 1 #click rate
+click_upgrade = 1 #click rate upgrade
 upgrade_cost = 25
+uc_rate = 8 #upgrade cost rate
 upgrade_level = 1
 
-def formatNum(number):
+
+def format_num(number):
     if abs(number) >= 1e15:
         return f'{number / 1/15:.2f}Q'
     elif abs(number) >= 1e12:
@@ -27,17 +31,17 @@ def formatNum(number):
 
 
 layout = [
-    [sg.Text('Click Count:', size=(15, 1)), sg.Text(click_count, size=(10, 1), key='COUNT')],
-    [sg.Button('Click!', key='CLICK')],
-    [sg.Text('Click Value: ', size=(15, 1)), sg.Text(click_value, size=(10, 1), key='VALUE')],
-    [sg.Button(f'Upgrade Click Value [Lv. {upgrade_level}] ({formatNum(upgrade_cost)} clicks)', key='UPGRADE')],
+    [sg.Text('')],
+    [sg.Text('Click Count:', size=(15, 1), font= font_name), sg.Text(click_count, size=(10, 1), key='COUNT', font= font_name)],
+    [sg.Button('Click!', key='CLICK', size= (10, 1), font= font_name)],
+    [sg.Text('Click Value: ', size=(15, 1), font= font_name), sg.Text(click_value, size=(10, 1), key='VALUE', font= font_name)],
+    [sg.Button(f'Upgrade Click Value [Lv. {upgrade_level}] ({format_num(upgrade_cost)} clicks)', key='UPGRADE', font= font_name)],
     [sg.Text('', key = 'COST')],
-    [sg.Button('Quit')]
+    [sg.Button('Quit', size = (10, 1), font= font_name)]
 ]
 
 
-
-window = sg.Window('Clicker Game GUI', layout, size = (1000,500))
+window = sg.Window('Clicker Game GUI', layout, size = (1200, 600), resizable= False, font= font_name)
 
 
 while True:
@@ -48,18 +52,25 @@ while True:
 
     if event == 'CLICK':
         click_count += click_value
-        window['COUNT'].update(formatNum(click_count))
+        window['COUNT'].update(format_num(click_count))
         window['COST'].update('')
     elif event == 'UPGRADE':
         if click_count >= upgrade_cost:
-            click_value += click_upgrade
+
+            click_value += r.randint(click_upgrade, click_upgrade+2)
             click_upgrade += 1
+
             click_count -= upgrade_cost
-            upgrade_cost += 8
+
+            upgrade_cost += uc_rate
+            uc_rate += r.randint(15, 50)
+
             upgrade_level += 1
-            window['VALUE'].update(formatNum(click_value))
-            window['COUNT'].update(formatNum(click_count))
-            window['UPGRADE'].update(f'Upgrade Click Value [Lv. {upgrade_level}] ({formatNum(upgrade_cost)} clicks)')
+
+            window['VALUE'].update(format_num(click_value))
+            window['COUNT'].update(format_num(click_count))
+            window['UPGRADE'].update(f'Upgrade Click Value [Lv. {upgrade_level}] ({format_num(upgrade_cost)} clicks)')
         else:
-            window['COST'].update('Not enough clicks.')
+            window['COST'].update('Not enough clicks.', font= font_name)
+
 window.close()
