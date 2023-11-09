b803e307ecfe29f64c3c012fb12782a10895a128
MicroWave78 <dragoschelariu64@gmail.com>
2023-11-09 02:29:04 +0200

    menu window completed, redesigned, button action needed

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ import time as t
 import random as r
 
 sg.theme('darkbrown2') 
+sg.set_options(font=('Leelawadee', 12), element_padding=(0,0))
 
-sg.set_options(font=('Leelawadee', 12))
+# click related variables
 click_count = 0
 click_value = 1 #click rate
 click_upgrade = 1 #click rate upgrade
@@ -12,10 +13,13 @@ upgrade_cost = 25
 uc_rate = 8 #upgrade cost ++
 upgrade_level = 1
 
+auto_cost = 1500
+
+
 # function to format big numbers
 def format_num(number):
     if abs(number) >= 1e15:
-        return f'{number / 1/15:.2f}Q'
+        return f'{number / 1e15:.2f}Q'
     elif abs(number) >= 1e12:
         return f'{number / 1e12:.2f}T'
     elif abs(number) >= 1e9:
@@ -29,26 +33,63 @@ def format_num(number):
 
 # layout for menu window
 menu_layout = [
-    [sg.Text('')],
-    [sg.Text(' ', size= (50,1)), sg.Text('Welcome To Clicker Game GUI\nBy Dragos Chelariu', font= ('Leelawadee', 14))]
+    [sg.Text(' ', size= (1,8))],
+
+    [sg.Text(' ', pad= (225,1)), sg.Text('Welcome To Clicker Game GUI', font= ('Leelawadee', 15, 'bold'))],
+    [sg.Text(' ', pad= (246,1)), sg.Text('By', font= ('Leelawadee', 15, 'bold')),
+                                 sg.Text('Dragos Chelariu', font= ('Leelawadee', 15, 'italic'))],
+
+    [sg.Text(' ')],
+    [sg.Text(' ', pad = (250,1)), sg.Button('Start Game', size= (15,2), button_color='silver', font= ('Leelawadee', 13,'bold'), key= 'Start')],
+
+    [sg.Text(' ')],
+    [sg.Text(' ', pad= (250,1)), sg.Button('Exit Game', size= (15,1), button_color= 'red', mouseover_colors='darkred', font= ('Leelawadee', 13))]
 ]
 
 # layout for main game window
 layout = [
+    [sg.Text()],
+    
+    [sg.Text('', pad = (50,0)),sg.Text('AutoClickers:', font= ('Leelawadee', 14, 'bold'), background_color= '#4a87a8'), 
+     sg.Text('', pad = (125,0)), sg.Text('Clicker Game GUI', font= ('Leelawadee', 16, 'bold'), background_color= '#e6a23e'),
+     sg.Text('', pad = (140,0)),sg.Text('Upgrades:', font= ('Leelawadee', 14, 'bold'), background_color= '#4a87a8')],
+
+    [sg.Text()],
+    [sg.Text('', pad= (30,15)), sg.Text('- T1\t'), sg.Button(f'Purchase [{format_num(auto_cost)}]'), 
+     sg.Text('', pad = (300,0)), sg.Text('- Adventurer\t'), sg.Button(f'Purchase [{format_num(auto_cost)}]')],
+
+    [sg.Text('', pad= (30,15)), sg.Text('- T2\t'), sg.Button(f'Purchase [{format_num(auto_cost*3)}]'),
+     sg.Text('', pad= (115,1)), sg.Text('Click Count: ', font= ('Leelawadee', 14, 'bold')), sg.Text(click_count, font= ('Leelawadee', 14, 'bold'), size=(10, 1), key='COUNT'),
+     sg.Text('', pad = (300,0)), sg.Text('- Hero\t\t'), sg.Button(f'Purchase [{format_num(auto_cost)}]')],
+
+    [sg.Text('', pad= (30,15)), sg.Text('- T3\t'), sg.Button(f'Purchase [{format_num(auto_cost*5)}]'),
+     sg.Text('', pad= (120,1)), sg.Button('Click!',font= ('Leelawadee', 13, 'bold'), key='CLICK', size= (10, 1)),
+     sg.Text('', pad = (300,0)), sg.Text('- Champion\t'), sg.Button(f'Purchase [{format_num(auto_cost)}]')],
+
+    [sg.Text('', pad= (30,15)), sg.Text('- T4\t'), sg.Button(f'Purchase [{format_num(auto_cost*10)}]'), 
+     sg.Text('', pad = (296,0)), sg.Text('- Challanger\t'), sg.Button(f'Purchase [{format_num(auto_cost)}]')],
+
+   
+
+    
     [sg.Text('')],
-    [sg.Text('Click Count:', size=(15, 1)), sg.Text(click_count, size=(10, 1), key='COUNT')],
-    [sg.Button('Click!', key='CLICK', size= (10, 1))],
-    [sg.Text('Click Value: ', size=(15, 1)), sg.Text(click_value, size=(10, 1), key='VALUE')],
-    [sg.Button(f'Upgrade Click Value [Lv. {upgrade_level}] ({format_num(upgrade_cost)} clicks)', key='UPGRADE')],
-    [sg.Text('', key = 'COST')],
-    [sg.Text('', size = (1, 18))],
+    [sg.Text('', pad = (255,0)), sg.Text('Click Value: ', size=(15, 1)), sg.Text(click_value, size=(10, 1), key='VALUE')],
+    [sg.Text('', pad = (210,0)), sg.Button(f'Upgrade Click Value [Lv. {upgrade_level}] ({format_num(upgrade_cost)} clicks)', key='UPGRADE')],
+    [sg.Text('', pad = (245,0)), sg.Text('', key = 'COST')],
+
+    [sg.Text('', pad= (0,25))],
+
     [sg.Button('Exit Game', size = (10, 1))]
+
+
 ]
 
-# open menu window
+window = sg.Window('Clicker Game GUI', layout, size = (1200, 600), resizable= False)    # main game window
 menu_window = sg.Window('Clicker Game GUI', menu_layout, size = (1200, 600), resizable= False)  # menu window
+
 check_play = True   # variable to check if the user wants to play or exit
 
+# open menu window
 while True:
     event, values = menu_window.read()
 
@@ -56,13 +97,15 @@ while True:
         check_play = False
         break
 
+    if event == 'Start':
+        break
 
 
 menu_window.close()
 
 
 # open main game window
-window = sg.Window('Clicker Game GUI', layout, size = (1200, 600), resizable= False)    # main game window
+
 while check_play == True:
     event, values = window.read()
 
