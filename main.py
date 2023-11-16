f85330e2a84c789058045400dd856ec8caa0e886
MicroWave78 <dragoschelariu64@gmail.com>
2023-11-16 13:50:09 +0200

    boss window now working, still need to design it

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import PySimpleGUI as sg
-import time as t
+import time 
 import random as r
 
 sg.theme('darkbrown2') 
@@ -15,13 +15,13 @@ adventurer = 0
 hero = 0
 champ = 0
 chall = 0
-total_u = adventurer+hero+chall+champ
+
 
 # upgrades cost
 adventurer_cost = 50
 hero_cost = 30000
-chall_cost = 10000000
-champ_cost = 2000000000
+chall_cost = 100000000
+champ_cost = 20000000000
 
 # upgrades click rate
 click_adventurer = 1
@@ -41,6 +41,11 @@ count = 0
 random_count = r.randint(1,100) # when to trigger diamond +1 (line 157)
 multi = diamonds*0.2    # multiplier for each diamond
 
+# boss variables
+boss_hp = 10
+boss_trigger = r.randint(1,10)
+boss_click_count = 0
+
 # function to format big numbers
 def format_num(number):
     if abs(number) >= 1e15:
@@ -55,9 +60,19 @@ def format_num(number):
         return f'{number / 1e3:.2f}K'
     else:
         return number
+    
+def get_gif_data(filename):
+    try:
+        with open(filename, "rb") as file:
+            return file.read()
+    except FileNotFoundError:
+        print(f"File not found: {filename}")
+        return b""
 
 # layout for menu window
 menu_layout = [
+
+    [sg.Image(key='gif')],
     [sg.Text(' ', size= (1,8))],
 
     [sg.Text(' ', pad= (225,1)), sg.Text('Welcome To Clicker Game GUI', font= ('Leelawadee', 15, 'bold'))],
@@ -71,9 +86,10 @@ menu_layout = [
     [sg.Text(' ', pad= (250,1)), sg.Button('Exit Game', size= (15,1), button_color= 'red', mouseover_colors='darkred', font= ('Leelawadee', 13))]
 ]
 
+
 # layout for main game window
 layout = [
-
+    
     [sg.Text()],
     [sg.Text('', pad = (50,0)),sg.Text('AutoClickers:', font= ('Leelawadee', 14, 'bold'), background_color= '#4a87a8'), 
      sg.Text('', pad = (120,0)), sg.Text('Clicker Game GUI', font= ('Leelawadee', 16, 'bold'), background_color= '#e6a23e'),
@@ -89,7 +105,7 @@ layout = [
      sg.Text('', pad= (115,1)), sg.Text('Click Count: ', font= ('Leelawadee', 14, 'bold')), sg.Text(click_count, font= ('Leelawadee', 14, 'bold'), size=(10, 1), key='COUNT'),
      sg.Text('', pad = (55,0)), sg.Text('- Hero:', background_color='#48db68', font=('Leelawadee', 13)), 
      sg.Text(' 0', key= 'hero_value'), sg.Text(' '*17), 
-     sg.Button(f'Purchase [{format_num(hero_cost)}]', key= 'hero')],
+     sg.Button(f'Purchase [{format_num(hero_cost)}]', auto_size_button=True, key= 'hero')],
 
     [sg.Text('', pad= (15,15)), sg.Text('- T3\t', key= 'T3'), sg.Button(f'Purchase [{format_num(T3_cost)}]'),
      sg.Text('', pad= (125,1)), sg.Button(f'Click!\n[{click_value}]',font= ('Leelawadee', 12, 'bold'), key='CLICK', size= (12, 2)),
@@ -109,27 +125,25 @@ layout = [
 
 ]
 
-boss_layout = [
-
-    
-]
-
 window = sg.Window('Clicker Game GUI', layout, size = (1200, 600), resizable= False, no_titlebar= True)    # main game window
-menu_window = sg.Window('Clicker Game GUI', menu_layout, size = (1200, 600), resizable= False, no_titlebar= True)  # menu window
+menu_window = sg.Window('Clicker Game GUI', menu_layout, size = (1200, 600), resizable= False, no_titlebar= False)  # menu window
+
 
 check_play = True   # variable to check if the user wants to play or exit
 
 # open menu window
 while True:
     
-    event, values = menu_window.read()
-
+    event, values = menu_window.read(timeout=100)
+    
     if event in (sg.WIN_CLOSED, 'Exit Game'):
         check_play = False
         break
-
+    
     if event == 'Start':
         break
+
+    
 
 menu_window.close()
 
@@ -137,6 +151,7 @@ menu_window.close()
 # open main game window
 while check_play == True:
 
+    total_u = adventurer+hero+chall+champ
     # changes color of CLICK button to random
     colors = ["#"+''.join([r.choice('0123456789ABCDEF') for j in range(6)])
              for i in range(50)]
@@ -147,13 +162,31 @@ while check_play == True:
     if event in (sg.WIN_CLOSED, 'Exit Game'):
         break
 
-    if event == 'CLICK':
-        click_count += click_value
-        count += 1
-        print(count)
-        window['COUNT'].update(format_num(click_count))
-        window['CLICK'].update(f'Click!\n[{format_num(click_value)}]', button_color= color_r)
-        
+    if boss_click_count == boss_trigger:
+        # boss window layout
+        boss_layout = [
+            [sg.ProgressBar(boss_hp, 'h', size=(20, 5), key='bossHP'),
+             sg.Text(f'{boss_hp}', key='bossHPvalue')],
+            [sg.Button('Attack')]
+        ]
+
+        boss_window = sg.Window('Boss Fight', boss_layout, size=(600, 300))     # boss window
+        while True:
+            event, values = boss_window.read()
+
+            if event == 'Attack':
+                boss_hp -= click_value
+                boss_window['bossHP'].update(boss_hp)
+                boss_window['bossHPvalue'].update(boss_hp)
+            
+            if boss_hp <= 0:
+                break
+
+        boss_click_count = 0    
+        boss_trigger = r.randint(1,200)
+        boss_hp = r.randint(click_value*30, click_value*100)
+        boss_window.close()
+
     if count == random_count:
         diamonds +=1
         
@@ -161,6 +194,17 @@ while check_play == True:
         count = 0
         random_count = r.randint(1,100)
         window['diamonds'].update(f'❖{format_num(diamonds)}', font=('Leelawadee', 15), text_color= 'aqua')
+
+    if event == 'CLICK':
+
+        click_count += click_value
+        count += 1  # clicks to diamond trigger
+        print(boss_click_count)
+
+        boss_click_count += 1
+
+        window['COUNT'].update(format_num(click_count))
+        window['CLICK'].update(f'Click!\n[{format_num(click_value)}]', button_color= color_r)
 
 # adventurer upgrade
     elif event == 'adventurer':
@@ -173,13 +217,14 @@ while check_play == True:
             click_value += adventurer
             click_count -= adventurer_cost
             
-            uc_rate += r.randint(100, 1000)
+            uc_rate += r.randint(100, 700)
             adventurer_cost += uc_rate
             
 
             window['COUNT'].update(format_num(click_count))
             window['adv_value'].update(f' {format_num(adventurer)}')
             window['adventurer'].update(f'Upgrade [{format_num(adventurer_cost)}]')
+            window['CLICK'].update(f'Click!\n[{format_num(click_value)}]')
 
 # hero upgrade
     elif event == 'hero':
@@ -187,17 +232,18 @@ while check_play == True:
 
             click_value -= hero
             hero += r.randint(click_hero+1, click_hero+80)
-            click_hero += 30
+            click_hero += 40
 
             click_value += hero
             click_count -= hero_cost
 
-            uc_rate += r.randint(1000, 5000)
+            uc_rate += r.randint(1000, 3000)
             hero_cost += uc_rate
 
             window['COUNT'].update(format_num(click_count))
             window['hero_value'].update(f' {format_num(hero)}')
             window['hero'].update(f'Upgrade [{format_num(hero_cost)}]')
+            window['CLICK'].update(f'Click!\n[{format_num(click_value)}]')
 
 # challanger upgrade
     elif event == 'challanger':
@@ -205,7 +251,7 @@ while check_play == True:
 
             click_value -= chall
             chall += r.randint(click_chall+1, click_chall+100)
-            click_chall += 80
+            click_chall += 100
 
             click_value += chall
             click_count -= chall_cost
@@ -216,6 +262,7 @@ while check_play == True:
             window['COUNT'].update(format_num(click_count))
             window['chall_value'].update(f' {format_num(chall)}')
             window['challanger'].update(f'Upgrade [{format_num(chall_cost)}]')
+            window['CLICK'].update(f'Click!\n[{format_num(click_value)}]')
 
 # champion upgrade
     elif event == 'champion':
@@ -234,4 +281,5 @@ while check_play == True:
             window['COUNT'].update(format_num(click_count))
             window['champ_value'].update(f' {format_num(champ)}')
             window['champion'].update(f'Upgrade [{format_num(champ_cost)}]')
+            window['CLICK'].update(f'Click!\n[{format_num(click_value)}]')
 window.close()
